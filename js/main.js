33a65d56822866846fae14d7ebd717c1fd83771d
Kellee Martins <kelleemartins@gmail.com>
2017-05-22 12:26:37 -0600

    Colors and naked cat images

--- js/main.js
+++ js/main.js
@@ -12,16 +12,50 @@ var app = angular.module('industryInnovators', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     // Home
-    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    .when("/", {templateUrl: "partials/home.html", controller: "CatController"})
     .when("/todo", {templateUrl: "partials/todo.html", controller: "TodoCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // else 404
-    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
 
-app.controller('KelleesController', ['$scope', function($scope) {
-	$scope.product = 'hello sunshine';
+app.controller('CatController', ['$scope', function($scope) {
+	$scope.cats = [
+		{
+			name: 'Fluffy',
+			image: 'http://cdn2-www.webecoist.momtastic.com/assets/uploads/2010/02/sphynx_cats_8x.jpg'
+		},
+		{
+			name: 'Sir Nakey',
+			image: 'https://qph.ec.quoracdn.net/main-qimg-4df06f1cfecbaed943e664531d52494f-c'
+		},
+		{
+			name: 'Pantless',
+			image: 'https://featuredcreature.com/wp-content/uploads/2011/12/DSC_0156.jpg'
+		},
+		{
+			name: 'Selena Gomez',
+			image: 'https://pbs.twimg.com/profile_images/586008601258405888/XggTWhbR.jpg'
+		},
+		{
+			name: 'Stimpy',
+			image: 'http://hairlesscatblog.com/wp-content/uploads/2013/03/canadian-hairless-cat-for-sale.jpg'
+		},
+		{
+			name: 'Fabio',
+			image: 'https://s-media-cache-ak0.pinimg.com/736x/80/df/37/80df374b87514c44651ff78330537dca.jpg'
+		},
+		{
+			name: 'Fluffy',
+			image: 'http://cdn2-www.webecoist.momtastic.com/assets/uploads/2010/02/sphynx_cats_8x.jpg'
+		},
+		{
+			name: 'Sir Nakey',
+			image: 'https://qph.ec.quoracdn.net/main-qimg-4df06f1cfecbaed943e664531d52494f-c'
+		},
+		{
+			name: 'Pantless',
+			image: 'https://featuredcreature.com/wp-content/uploads/2011/12/DSC_0156.jpg'
+		},
+	];
 }]);
 
 
